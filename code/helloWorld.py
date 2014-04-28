4bd4ce786c1f893c818f89a1f441bbc2ac887f60
Danielle Smith <miss.d.smith@gmail.com>
2014-04-28 15:51:13 +0100

    Added different logging objects.

--- code/helloWorld.py
+++ code/helloWorld.py
@@ -1,4 +1,11 @@
-from psychopy import visual, core, event
+from psychopy import visual, core, event, logging
+
+logging.console.setLevel(logging.EXP) # add logging objects
+logFile = logging.LogFile('lastrun.log','a') # methods in psychopy are camelCase, class is AlternateCase; a for append
+logFile.setLevel(logging.DEBUG) # can have different logging levels for different targets
+logging.warn("send a warning")
+logging.error("you screwed up")
+logging.debug("just checking you know that this is happening")
 
 win = visual.Window([400,400]) # set up a window that is 400px by 400px
 txt = visual.TextStim(win, text = "Hello World!")
