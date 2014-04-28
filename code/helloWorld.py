a72ffe1eaf4d5aed124de6e6bfdd62c151dfd52b
Danielle Smith <miss.d.smith@gmail.com>
2014-04-28 14:40:33 +0100

    Made stimuli dynamic.

--- code/helloWorld.py
+++ code/helloWorld.py
@@ -4,6 +4,7 @@ win = visual.Window([400,400]) # set up a window that is 400px by 400px
 txt = visual.TextStim(win, text = "Hello World!")
 
 for frameN in range(60): # present text for exactly 60 frames
+    txt.setOri(frameN) # make stimuli dynamic
     txt.draw()
     win.flip()
 
