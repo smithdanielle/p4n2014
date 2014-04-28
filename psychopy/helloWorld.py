542d39f0c3e3d54294a9601f1c6ed669da997fa2
Danielle Smith <miss.d.smith@gmail.com>
2014-04-28 14:15:59 +0100

    Switched to timing by frames

--- psychopy/helloWorld.py
+++ psychopy/helloWorld.py
@@ -1,9 +1,10 @@
 from psychopy import visual, core, event
 
-win = visual.Window([400,400])
+win = visual.Window([400,400]) # set up a window that is 400px by 400px
 txt = visual.TextStim(win, text = "Hello World!")
 
-txt.draw()
-win.flip()
-core.wait(2)
+for frameN in range(60): # present text for exactly 60 frames
+    txt.draw()
+    win.flip()
+
 win.close()
