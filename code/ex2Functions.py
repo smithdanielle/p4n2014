569f05e1d04c47cd708a9920399c95d03f4523c5
Danielle Smith <miss.d.smith@gmail.com>
2014-04-29 09:27:07 +0100

    Initial commit of untouched Exercise 2: Functions and Errors script.

---
+++ code/ex2Functions.py
@@ -0,0 +1,33 @@
+# Load psychopy modules
+from psychopy import visual, event, core
+win = visual.Window(size=[500, 500])
+text = visual.TextStim(win)
+
+# Messages
+welcome = 'hi and welcome to this experiment'
+instruction = 'really, there\'s not much to do. We just show a bunch of messages. Press RETURN to continue.'
+thanks  'thank you and goodbye. We hope you enjoyed!'
+
+# Show welcome message
+text.setText(welcome)  # set text
+text.draw()  # prepare to show it
+win.flip()  # actually show it
+response = event.waitKeys(keyList=None)  # accepts all keys
+if response[0] == 'q':  # response[0] is the name of the first pressed key
+    core.quit()  # quit if 'q' was pressed
+
+# Show instruction
+text.setText(instruction)
+text.draw()
+win.flip()
+response = event.waitKeys(keyList=['return', 'q'])  # listen for return and q only
+if response[1] == 'q':
+    core.quit()
+
+# Show debriefing
+text.setText(thanks)
+text.draw()
+win.flip()
+response = event.waitKeys#(keyList=None)  # accepts all keys
+if response[0] == 'q':
+    core.quit()
