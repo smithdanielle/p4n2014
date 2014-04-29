7914ad091bd3d3c588585e1ca9cda75d3e0893f8
Danielle Smith <miss.d.smith@gmail.com>
2014-04-29 16:21:22 +0100

    Used `copy` to allow for a central version of rds stimulus.

--- code/stereoacuity.py
+++ code/stereoacuity.py
@@ -1,7 +1,9 @@
 from psychopy import visual, core, event, data, gui
+import copy
 
 # set things that don't change throughout the experiment
-win = visual.Window([400,400], fullscr=False, units='deg', monitor='default')
+win = visual.Window([800,800], fullscr=False, units='deg', 
+    monitor='default', blendMode='add')
 globalClock = core.Clock()
 respClock = core.Clock()
 
@@ -21,15 +23,18 @@ fileName = "data/{participant}_{dateStr}".format(**info)
 fixation = visual.Circle(win, size = 0.25,
     lineColor = 'white', fillColor = 'lightGrey')
 
-rds = visual.DotStim(win, nDots=500, fieldSize=(2,2), 
-    fieldShape='sqr', dotLife= -1, speed = 0.0, color='white')
+rds = visual.DotStim(win, nDots=500, fieldSize=(4,4), 
+    fieldShape='sqr', dotLife= -1, speed = 0.0)
 
-#create the staircase handler
-#staircase = data.StairHandler(startVal = 20.0,
-#                          stepType = 'db', stepSizes=[8,4,4,2,2,1,1],
-#                          nUp=1, nDown=3,  #will home in on the 80% threshold
+rdsLeft = copy.copy(rds)
+rdsLeft.setColor([1.0,0,0])
+rdsLeft.setFieldPos([-0.15,0])
+rdsRight=copy.copy(rds)
+rdsRight.setColor([0,0.6,0.6])
+rdsRight.setFieldPos([0.15,0])
 
-rds.draw()
+rdsLeft.draw()
+rdsRight.draw()
 
 win.flip()
 
