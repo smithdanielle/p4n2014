9dc42ae436d7b8278a06028fcc6dc9ef43bd1cc7
Danielle Smith <miss.d.smith@gmail.com>
2014-04-29 14:14:23 +0100

    Set display units to degrees of visual angle, not pixels.

--- code/stereoacuity.py
+++ code/stereoacuity.py
@@ -1,7 +1,7 @@
 from psychopy import visual, core, event, data, gui
 
 # set things that don't change throughout the experiment
-win = visual.Window([400,400], fullscr=False, units='pix')
+win = visual.Window([400,400], fullscr=False, units='deg', monitor='default')
 globalClock = core.Clock()
 respClock = core.Clock()
 
@@ -14,15 +14,23 @@ if not dlg.OK:
 info['dateStr'] = data.getDateStr() #will create str of current date/time
 
 #create the base filename for our data files
-filename = "data/{participant}_{dateStr}".format(**info)
+fileName = "data/{participant}_{dateStr}".format(**info)
+#dataFile = open(fileName+'.csv', 'w')#a simple text file with 'comma-separated-values'
+
 # initialise stimuli
-fixation = visual.Circle(win, size = 5,
+fixation = visual.Circle(win, size = 0.25,
     lineColor = 'white', fillColor = 'lightGrey')
 
-rds = visual.DotStim(win, nDots=500, fieldSize=(100,100), 
+rds = visual.DotStim(win, nDots=500, fieldSize=(2,2), 
     fieldShape='sqr', dotLife= -1, speed = 0.0, color='white')
+
+#create the staircase handler
+#staircase = data.StairHandler(startVal = 20.0,
+#                          stepType = 'db', stepSizes=[8,4,4,2,2,1,1],
+#                          nUp=1, nDown=3,  #will home in on the 80% threshold
 
 rds.draw()
 
 win.flip()
+
 event.waitKeys()
